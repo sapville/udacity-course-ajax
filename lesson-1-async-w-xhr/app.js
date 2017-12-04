d101f6bf787b1bd5bd9f89633da46679ba48211c
Evgenii Fedoseev <abap@sapville.com>
2017-12-04 19:12:51 +0300

    combine an image and articles

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -4,73 +4,72 @@
   searchField.value = 'hippo';
   let searchedForText;
   const responseContainer = document.querySelector('#response-container');
-  const c_mode = {
-    nyt: 'nyt',
-    unsplash: 'unsplash'
-  };
-  let mode = c_mode.nyt;
 
   form.addEventListener('submit', function (e) {
     e.preventDefault();
     let searchRequest = '';
     searchedForText = searchField.value;
-    mode = document.getElementById('nyt').checked ? c_mode.nyt : c_mode.unsplash;
-    const req = new XMLHttpRequest();
-    switch (mode) {
-      case c_mode.nyt:
-        searchRequest = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=faaff1099919450984229eae4d765e1a`;
-        req.onload = addArticles;
-        break;
-      case c_mode.unsplash:
-        searchRequest = `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`;
-        req.onload = addImages;
-        break;
-    }
-    req.open('GET', searchRequest);
-    if (mode === c_mode.unsplash) {
-      req.setRequestHeader('Authorization', 'Client-ID 14d5810d436b6e09a64261d8b3d8271645affec5c67a0633fb52a7002b071fcb');
-    }
-    req.onerror = () => {
-      responseContainer.innerHTML = `
-      <p>Error occurred during the request</p>
-    `;
-    };
-    req.send();
-  });
 
-  function addArticles () {
-    responseContainer.innerHTML = '';
-    JSON.parse(this.responseText).response.docs.forEach(addArticle);
-  }
+    const imgLoad = new Promise((resolve) => {
+      const imgReq = new XMLHttpRequest();
+      imgReq.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+      imgReq.setRequestHeader('Authorization', 'Client-ID 14d5810d436b6e09a64261d8b3d8271645affec5c67a0633fb52a7002b071fcb');
+      imgReq.onload = addImage;
+      imgReq.onerror = function () {
+        responseContainer.innerHTML = '<p>Error occurred during the request</p>';
+        resolve();
+      };
+      imgReq.send();
 
-  function addArticle (data) {
-    let containerContent = '';
-    if (!data) {
-      containerContent = '<p>No article found</p>';
-    } else {
-      console.log(data);
-      containerContent = `
-      
-      `;
-    }
-  }
-
-  function addImages () {
-    responseContainer.innerHTML = '';
-    JSON.parse(this.responseText).results.forEach(addImage);
-  }
-
-  function addImage (data) {
-    let containerContent = '';
-    if (!data) {
-      containerContent = '<p class="error-no-image">No image found</p>';
-    } else {
-      containerContent = `
+      function addImage () {
+        let containerContent = '';
+        const data = JSON.parse(this.responseText).results[0];
+        responseContainer.innerHTML = '';
+        if (!data) {
+          containerContent = '<p class="error-no-image">No image found</p>';
+        } else {
+          containerContent = `
         <figure>
             <img src="${data.urls.regular}" alt="${searchedForText}">
             <figcaption>Photo by ${data.user.first_name} ${data.user.last_name}</figcaption>
         </figure>
       `;
+        }
+        responseContainer.insertAdjacentHTML('beforeend', containerContent);
+        resolve();
+      }
+    });
+
+    imgLoad.then( () => {
+      const artReq = new XMLHttpRequest();
+      artReq.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=faaff1099919450984229eae4d765e1a`);
+      artReq.onload = addArticles;
+      artReq.onerror = reqError;
+      artReq.send();
+    });
+
+
+  });
+
+  function reqError () {
+    responseContainer.innerHTML = '<p>Error occurred during the request</p>';
+  }
+
+  function addArticles () {
+    const data = JSON.parse(this.responseText);
+    let containerContent = '';
+    if (!data || !data.response.docs || data.response.docs.length < 1) {
+      containerContent = '<div class="error-no-articles">No articles found</div>';
+    } else {
+      // console.log(data);
+      containerContent =
+        '<ul>' +
+          data.response.docs.map(article => `
+            <li class="article">
+                <h2><a href="${article.web_url}">${article.headline.main}</a> </h2>
+                <p>${article.snippet}</p>
+            </li>`).join('')
+        + '</ul>';
     }
     responseContainer.insertAdjacentHTML('beforeend', containerContent);
   }
